1b93fe289fcb7f165250350abbaebad28717acf6
ningerson2002 <ingersonmeacham01@allegheny.edu>
2023-11-29 17:48:05 -0500

    Resolve imports and initialize window
    Import dependencies, initialize window, window size, and window title.

---
+++ weather_app.py
@@ -0,0 +1,11 @@
+from tkinter import *
+import requests
+import json
+from datetime import datetime
+
+# Initialize window
+root = Tk()
+root.geometry("400x400")  # size of window (default)
+root.resizable(0, 0)  # fixed window size
+
+root.title("Weather App -- Nic Ingerson")
